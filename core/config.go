87509c4bb985b7dee170cd78da3878573bc80c6e
Ekene Izukanne <ekeneizukanne@gmail.com>
2019-12-23 07:55:59 +0100

    add initial Virgo blockchain configuration

---
+++ core/config.go
@@ -0,0 +1,10 @@
+package core
+
+const (
+	blockGenerationInterval = 10 // seconds
+	difficultyAdjustmentInterval = 10 // blocks
+	maxBlockNumber = 500000000
+	publicAddressBase58Prefix = "" //TODO Currency-specific address prefix
+	moneySupply = 0 //TODO Specify total number of available coins
+	minimumFee = 0 //TODO Define minimum fee for transactions
+)
